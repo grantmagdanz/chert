550db9ade84a12a6556e0407763fced5e50a0bbc
Konrad Feiler <konrad@tactica.de>
2015-08-25 18:18:53 +0200

    Update to Swift 2.0 and merge pull requests

--- Keyboard/SnapBoard.swift
+++ Keyboard/SnapBoard.swift
@@ -27,43 +27,42 @@ class Snapboard: KeyboardViewController {
     }
     
     override func keyPressed(key: Key) {
-        if let textDocumentProxy = self.textDocumentProxy as? UITextDocumentProxy {
-            let keyOutput = key.outputForCase(self.shiftState.uppercase())
-            
-            if key.type == .Character || key.type == .SpecialCharacter {
-                let context = textDocumentProxy.documentContextBeforeInput
-                if context != nil {
-                    if count(context) < 2 {
-                        textDocumentProxy.insertText(keyOutput)
-                        return
-                    }
-                    
-                    var index = context!.endIndex
-                    
-                    index = index.predecessor()
-                    if context[index] != " " {
-                        textDocumentProxy.insertText(keyOutput)
-                        return
-                    }
-                    
-                    index = index.predecessor()
-                    if context[index] == " " {
-                        textDocumentProxy.insertText(keyOutput)
-                        return
-                    }
+        let textDocumentProxy = self.textDocumentProxy
+        let keyOutput = key.outputForCase(self.shiftState.uppercase())
+        
+        if key.type == .Character || key.type == .SpecialCharacter {
+            let context = textDocumentProxy.documentContextBeforeInput
+            if context != nil {
+                if context!.characters.count < 2 {
+                    textDocumentProxy.insertText(keyOutput)
+                    return
+                }
+                
+                var index = context!.endIndex
+                
+                index = index.predecessor()
+                if context![index] != " " {
+                    textDocumentProxy.insertText(keyOutput)
+                    return
+                }
+                
+                index = index.predecessor()
+                if context![index] == " " {
+                    textDocumentProxy.insertText(keyOutput)
+                    return
+                }
 
-                    textDocumentProxy.insertText(keyOutput)
-                    return
-                }
-                else {
-                    textDocumentProxy.insertText(keyOutput)
-                    return
-                }
+                textDocumentProxy.insertText(keyOutput)
+                return
             }
             else {
                 textDocumentProxy.insertText(keyOutput)
                 return
             }
+        }
+        else {
+            textDocumentProxy.insertText(keyOutput)
+            return
         }
     }
     
@@ -92,7 +91,7 @@ class Snapboard: KeyboardViewController {
     }
     
     func takeScreenshotDelay() {
-        var timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("takeScreenshot"), userInfo: nil, repeats: false)
+        let timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("takeScreenshot"), userInfo: nil, repeats: false)
     }
     
     func takeScreenshot() {
@@ -102,15 +101,15 @@ class Snapboard: KeyboardViewController {
             let oldViewColor = self.view.backgroundColor
             self.view.backgroundColor = UIColor(hue: (216/360.0), saturation: 0.05, brightness: 0.86, alpha: 1)
             
-            var rect = self.view.bounds
+            let rect = self.view.bounds
             UIGraphicsBeginImageContextWithOptions(rect.size, true, 0)
-            var context = UIGraphicsGetCurrentContext()
+            let context = UIGraphicsGetCurrentContext()
             self.view.drawViewHierarchyInRect(self.view.bounds, afterScreenUpdates: true)
-            var capturedImage = UIGraphicsGetImageFromCurrentImageContext()
+            let capturedImage = UIGraphicsGetImageFromCurrentImageContext()
             UIGraphicsEndImageContext()
             let name = (self.interfaceOrientation.isPortrait ? "Screenshot-Portrait" : "Screenshot-Landscape")
-            var imagePath = "/Users/archagon/Documents/Programming/OSX/RussianPhoneticKeyboard/External/tasty-imitation-keyboard/\(name).png"
-            UIImagePNGRepresentation(capturedImage).writeToFile(imagePath, atomically: true)
+            let imagePath = "/Users/archagon/Documents/Programming/OSX/RussianPhoneticKeyboard/External/tasty-imitation-keyboard/\(name).png"
+            UIImagePNGRepresentation(capturedImage)!.writeToFile(imagePath, atomically: true)
             
             self.view.backgroundColor = oldViewColor
         }
